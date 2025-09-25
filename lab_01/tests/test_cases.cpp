bd2565a6134e05196be52cd224da71de4ed59280
KirillBelianskiy <belianskiykirill@gmail.com>
2025-09-25 16:37:40 +0300

    Fix test_cases.cpp

--- lab_01/tests/test_cases.cpp
+++ lab_01/tests/test_cases.cpp
@@ -2,43 +2,35 @@
 #include "../src/function.h"
 
 TEST(CalculateSumRangeTest, HandlesExampleRange) {
-    Solution solution;
-    EXPECT_EQ(11, solution.calculate_sum_range(2, 7));
+    EXPECT_EQ(11, calculate_sum_range(2, 7));
 }
 
 TEST(CalculateSumRangeTest, HandlesSingleNumberRange) {
-    Solution solution;
-    EXPECT_EQ(2, solution.calculate_sum_range(3, 3));
-    EXPECT_EQ(1, solution.calculate_sum_range(8, 8));
+    EXPECT_EQ(2, calculate_sum_range(3, 3));
+    EXPECT_EQ(1, calculate_sum_range(8, 8));
 }
 
 TEST(CalculateSumRangeTest, ThrowsExceptionOnInvalidInput) {
-    Solution solution;
-    EXPECT_THROW(solution.calculate_sum_range(10, 5), InvalidInput);
-    EXPECT_THROW(solution.calculate_sum_range(-5, 2), InvalidInput);
+    EXPECT_THROW(calculate_sum_range(10, 5), std::invalid_argument);
+    EXPECT_THROW(calculate_sum_range(-5, 2), std::invalid_argument);
 }
 
 TEST(CalculateSumRangeTest, RangeIncludingZero) {
-    Solution solution;
-    EXPECT_EQ(2, solution.calculate_sum_range(0, 2));
+    EXPECT_EQ(2, calculate_sum_range(0, 2));
 }
 
 TEST(CalculateSumRangeTest, RangeIsOnlyZero) {
-    Solution solution;
-    EXPECT_EQ(0, solution.calculate_sum_range(0, 0));
+    EXPECT_EQ(0, calculate_sum_range(0, 0));
 }
 
 TEST(CalculateSumRangeTest, RangeWithPowersOfTwo) {
-    Solution solution;
-    EXPECT_EQ(4, solution.calculate_sum_range(7, 8));
+    EXPECT_EQ(4, calculate_sum_range(7, 8));
 }
 
 TEST(CalculateSumRangeTest, RangeWithAllBitsSet) {
-    Solution solution;
-    EXPECT_EQ(7, solution.calculate_sum_range(14, 15));
+    EXPECT_EQ(7, calculate_sum_range(14, 15));
 }
 
 TEST(CalculateSumRangeTest, LargerRange) {
-    Solution solution;
-    EXPECT_EQ(32, solution.calculate_sum_range(0, 15));
+    EXPECT_EQ(32, calculate_sum_range(0, 15));
 }
